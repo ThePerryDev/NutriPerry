bb2d5d813face60374c56f7a55331a588c07e202
dani-dornas <lauby.dd@gmail.com>
2024-09-04 00:40:58 -0300

    Tela de login inicial

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,11 @@
+import Botao from "./Botao";
+import Input from "./Input";
+import InputWithIcons from "./InputWithIcons";
+import Titulo from "./Titulo";
+
+export { 
+    Titulo,
+    Input,
+    InputWithIcons,
+    Botao
+}
