d6221e95a13998534f803f4fe9da60da4be59013
dani-dornas <lauby.dd@gmail.com>
2024-10-07 17:28:07 -0300

    autenticação com falhas de frontend

--- frontend/src/screen/Cadastro/perfil_pessoal/CadastroAlturaPeso/CadastroAlturaPeso.tsx
+++ frontend/src/screen/Cadastro/perfil_pessoal/CadastroAlturaPeso/CadastroAlturaPeso.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { View, Text, TouchableOpacity, TextInput, Image } from "react-native";
 import { imagem4, setaVolta } from "../../../../assets";
 import styles from "./Styles";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../../../types/rootStack";
+import { AuthContext } from "../../../../context/auth/AuthContext";
+import user from "../../../../services/userService";
 
 type ContinuarScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -15,8 +17,36 @@ type Props = {
 };
 
 const CadastroAlturaPeso: React.FC<Props> = ({ navigation }) => {
-  const [altura, setAltura] = useState<string>("");
-  const [peso, setPeso] = useState<string>("");
+  const auth = useContext(AuthContext);
+  const [height, setHeight] = useState<string>("");
+  const [weight, setWeight] = useState<string>("");
+
+  const handleRegister = async () => {
+    if (height && weight) {
+      const heightNumber = parseFloat(height); // Convertendo o valor de altura para número
+      const weightNumber = parseFloat(weight); // Convertendo o valor de peso para número
+      const birthdate = new Date(2020, 1, 2);
+
+      if (!isNaN(heightNumber) && !isNaN(weightNumber)) {
+        await user.post({
+          height: heightNumber,
+          weight: weightNumber,
+          email: "",
+          password: "",
+          name: "",
+          activityLevel: "sedentario",
+          gender: "masculino",
+          goal: "perda de peso",
+          birthdate: birthdate,
+          isLogged: false,
+        });
+      } else {
+        alert("Insira valores numéricos válidos");
+      }
+    } else {
+      alert("Preencha todos os campos");
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -27,21 +57,24 @@ const CadastroAlturaPeso: React.FC<Props> = ({ navigation }) => {
         <Text style={{ fontSize: 20 }}>(4/5)</Text>
       </View>
       <Image source={imagem4} style={styles.image} resizeMode="contain" />
-      <Text style={styles.textgeral}>Insira seu altura</Text>
+      <Text style={styles.textgeral}>Insira seu height</Text>
       <TextInput
-        value={altura}
-        onChangeText={(altura) => setAltura(altura)}
+        value={height}
+        onChangeText={(height) => setHeight(height)}
         style={styles.input}
         keyboardType="numeric"
       />
-      <Text style={styles.textgeral}>Insira sua peso</Text>
+      <Text style={styles.textgeral}>Insira sua weight</Text>
       <TextInput
-        value={peso}
-        onChangeText={(peso) => setPeso(peso)}
+        value={weight}
+        onChangeText={(weight) => setWeight(weight)}
         style={styles.input}
         keyboardType="numeric"
       />
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate("TelaPPObjetivo")}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("TelaPPObjetivo")}
+      >
         <Text style={{ color: "#FFFFFF", fontSize: 30 }}>CONTINUAR</Text>
       </TouchableOpacity>
     </View>
--- frontend/src/screen/login/Login.tsx
+++ frontend/src/screen/login/Login.tsx
@@ -1,9 +1,12 @@
-import { View, StyleSheet, Button, Text } from 'react-native';
-import { InputWithIcons } from '../../components';
-import icone_perfil from "../../assets/perfil.png"
+import { View, Text, TouchableOpacity } from "react-native";
+import { InputWithIcons } from "../../components";
+import icone_perfil from "../../assets/perfil.png";
 import icone_senha from "../../assets/senha.png";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../types/rootStack";
+import styles from "../Cadastro/perfil_nutricionista/tela_nutri_formacao/styles";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/auth/AuthContext";
 
 type ContinuarScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -14,26 +17,55 @@ type Props = {
   navigation: ContinuarScreenNavigationProp;
 };
 
+const TelaLogin: React.FC<Props> = ({ navigation }) => {
 
-const TelaLogin: React.FC<Props> = ({ navigation }) =>{
+  const auth = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    // verifica se email e senha estão preeenchidos e manda para o contexto email e senha
+    if (email && password) {
+      const isLogged = await auth.signin(email, password);
+      if (isLogged) {
+        alert('logou')
+        navigation.navigate("CadastroNutriTelefone");
+      } else {
+        alert("Falha ao logar");
+      }
+    }
+  };
+  
   return (
-    <View style={styles.container}>
-      <InputWithIcons iconSource={icone_perfil} placeholder='Insira seu email...'/>
-      <InputWithIcons iconSource={icone_senha} placeholder='Insira sua senha...'/>
-      
-      <Button title='ENTRAR' onPress={() => navigation.navigate("CadastroNome")}/>
-      <Text>Ou cadastre-se...</Text>
+    <View style={styles.caixinha}>
+      <InputWithIcons
+        iconSource={icone_perfil}
+        placeholder="Insira seu email..."
+        value={email}
+        onChangeText={(email) => setEmail(email)}
+      />
+      <InputWithIcons
+        iconSource={icone_senha}
+        placeholder="Insira sua senha..."
+        value={password}
+        onChangeText={(password) => setPassword(password)}
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleLogin}
+      >
+        <Text style={{ color: "#FFFFFF", fontSize: 30 }}>ENTRAR</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("CadastroNutriTelefone")}
+      >
+        <Text style={{ color: "#FFFFFF", fontSize: 30 }}>CADASTRE-SE</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 export default TelaLogin;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
