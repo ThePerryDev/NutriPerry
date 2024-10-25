f85f556ff1afb61c1f7ae9584c1a64e103a0b3b0
heclair <heclair@hotmail.com>
2024-10-25 16:38:58 -0300

    total kcal sendo exibido abaixo de cada refeição na tela suas dietas

--- frontend/src/screen/Dietas/MenuDietas/MenuDietas.tsx
+++ frontend/src/screen/Dietas/MenuDietas/MenuDietas.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useContext, useState } from "react";
 import {
   View,
   Text,
@@ -14,6 +14,10 @@ import { camera, setaVolta } from "../../../assets";
 import styles from "./styles";
 import { Ionicons } from "@expo/vector-icons";
 import NewDietButton from "../../../components/Cadastro/NovaDieta/novadieta";
+import moment from "moment";
+import { AuthContext } from "../../../context";
+import axios from "axios";
+import { useFocusEffect } from "@react-navigation/native";
 
 type ContinuarScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -26,7 +30,99 @@ type Props = {
 
 const MenuDietas: React.FC<Props> = ({ navigation }) => {
   const [namediet, setNameDiet] = useState<string>("");
+  const [totalKcalCafe, setTotalKcalCafe] = useState(0);
+  const [totalKcalAlmoco, setTotalKcalAlmoco] = useState(0);
+  const [totalKcalJantar, setTotalKcalJantar] = useState(0);
+  const [totalKcalLanche, setTotalKcalLanche] = useState(0);
+  const { user } = useContext(AuthContext);
+  const [refreshing, setRefreshing] = useState(false);
 
+  const fetchCafedaManha = async () => {
+    try {
+      const responseTotais = await axios.get("http://192.168.1.4:3000/consumos/listarconsumo", {
+        params: {
+          userId: user?.id,
+          data: moment().format("YYYY-MM-DD"),
+          tipoRefeicao: "cafe_da_manha",
+        },
+      });
+
+      setTotalKcalCafe(responseTotais.data.totalKcal);
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+    }
+  };
+
+  const fetchAlmoco = async () => {
+    try {
+      const responseTotais = await axios.get("http://192.168.1.4:3000/consumos/listarconsumo", {
+        params: {
+          userId: user?.id,
+          data: moment().format("YYYY-MM-DD"),
+          tipoRefeicao: "almoco",
+        },
+      });
+
+      setTotalKcalAlmoco(responseTotais.data.totalKcal);
+
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+    }
+  };
+
+  const fetchJantar = async () => {
+    try {
+      const responseTotais = await axios.get("http://192.168.1.4:3000/consumos/listarconsumo", {
+        params: {
+          userId: user?.id,
+          data: moment().format("YYYY-MM-DD"),
+          tipoRefeicao: "jantar",
+        },
+      });
+
+      setTotalKcalJantar(responseTotais.data.totalKcal);
+
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+    }
+  };
+
+  const fetchLanche = async () => {
+    try {
+      const responseTotais = await axios.get("http://192.168.1.4:3000/consumos/listarconsumo", {
+        params: {
+          userId: user?.id,
+          data: moment().format("YYYY-MM-DD"),
+          tipoRefeicao: "lanches",
+        },
+      });
+      
+
+      setTotalKcalLanche(responseTotais.data.totalKcal);
+
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+    }
+  };
+
+  const onRefresh = () => {
+    setRefreshing(true);
+    fetchCafedaManha().then(() => setRefreshing(false));
+    fetchAlmoco().then(() => setRefreshing(false));
+    fetchJantar().then(() => setRefreshing(false));
+    fetchLanche().then(() => setRefreshing(false));
+  };
+
+  // Chama a função toda vez que a tela for focada
+  useFocusEffect(
+    useCallback(() => {
+      fetchCafedaManha();
+      fetchAlmoco();
+      fetchJantar();
+      fetchLanche();      
+      
+    }, [])
+  );
   
 
   return (
@@ -49,11 +145,32 @@ const MenuDietas: React.FC<Props> = ({ navigation }) => {
         />
       </View>
       <ScrollView>
-        {["Café da Manhã", "Almoço", "Jantar", "Lanches"].map((meal, index) => (
+  {["Café da Manhã", "Almoço", "Jantar", "Lanches"].map((meal, index) => {
+    let totalKcal = 0;
+
+    // Associando o total de calorias correto para cada refeição
+    switch (meal) {
+      case "Café da Manhã":
+        totalKcal = totalKcalCafe;
+        break;
+      case "Almoço":
+        totalKcal = totalKcalAlmoco;
+        break;
+      case "Jantar":
+        totalKcal = totalKcalJantar;
+        break;
+      case "Lanches":
+        totalKcal = totalKcalLanche;
+        break;
+      default:
+        totalKcal = 0;
+    }
+
+        return (
           <View key={index} style={styles.mealItem}>
             <View style={styles.mealInfo}>
               <Text style={styles.mealName}>{meal}</Text>
-              <Text style={styles.mealDetail}>Sem cardápio cadastrado</Text>
+              <Text style={styles.mealDetail}>{totalKcal} kcal</Text>
             </View>
             <TouchableOpacity
               onPress={() => {
@@ -78,8 +195,10 @@ const MenuDietas: React.FC<Props> = ({ navigation }) => {
               <Ionicons name="add-circle-outline" size={28} color="green" />
             </TouchableOpacity>
           </View>
-        ))}
-      </ScrollView>
+        );
+      })}
+    </ScrollView>
+
       {/* 
       <View style={styles.dietbuttoncontainer}>
         <NewDietButton
@@ -95,3 +214,7 @@ const MenuDietas: React.FC<Props> = ({ navigation }) => {
 };
 
 export default MenuDietas;
+function setRefreshing(arg0: boolean) {
+  throw new Error("Function not implemented.");
+}
+
