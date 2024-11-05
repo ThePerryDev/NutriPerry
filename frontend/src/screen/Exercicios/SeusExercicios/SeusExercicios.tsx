60cca3e2edcb0a6d3866062734c51524e4fd8978
Franciele Chesere <francielechesere0@gmail.com>
2024-11-04 23:16:38 -0300

    removendo checkbox

--- frontend/src/screen/Exercicios/SeusExercicios/SeusExercicios.tsx
+++ frontend/src/screen/Exercicios/SeusExercicios/SeusExercicios.tsx
@@ -1,12 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ScrollView, TextInput, TouchableOpacity, View, Text, Image, } from "react-native";
-import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { lupa, setaVolta } from "../../../assets";
 import styles from "./styles"
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../../../types/rootStack";
 import MenuInferior from "../../../components/MenuInferior/MenuInferior";
-import Checkbox from "expo-checkbox";
 import moment from "moment";
 import axios from "axios";
 import { AuthContext } from "../../../context";
@@ -23,12 +21,12 @@ type Props = {
 };
 
 type Exercicios = {
-  id: number;
-  data: string;
+  //id: number;
+  //data: string;
   atividadeFisica: string;
   gastoCalorico: number;
-  tempo: number;
-  userID: string;
+  //tempo: number;
+  //userID: string;
 }
 
 
@@ -36,6 +34,8 @@ const SeusExercicios: React.FC<Props> = ({ navigation }) => {// Estado para rast
   const [checkedItems, setCheckedItems] = useState<boolean[]>([]);
   const [exercicios, setExercicios] = useState<{ atividadeFisica: string, gastoCalorico: number }[]>([]); // Estado para armazenar os produtos
   const { user } = useContext(AuthContext);
+  const [selectedExercise, setSelectedExercise] = useState<Exercicios | null>(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const fetchExercicios = async () => {
     try {
@@ -50,12 +50,12 @@ const SeusExercicios: React.FC<Props> = ({ navigation }) => {// Estado para rast
       });
 
       const exercicios = responseExercicios.data.map((exercicio: any) => ({
-        //id: exercicio.id,
-        //data: exercicio.data,
+        id: exercicio.id,
+        data: exercicio.data,
         atividadeFisica: exercicio.atividadeFisica,
         gastoCalorico: exercicio.gastoCalorico,
-        //tempo: exercicio.tempo,
-        //userID: exercicio.userID,
+        tempo: exercicio.tempo,
+        userID: exercicio.userID,
       }));
 
       setExercicios(exercicios); // Atualiza o estado com os dados recebidos
@@ -69,13 +69,39 @@ const SeusExercicios: React.FC<Props> = ({ navigation }) => {// Estado para rast
   useEffect(() => {
     fetchExercicios();
   }, []);
-  
-    // Função para alterar o estado de um checkbox específico
-    const handleCheckboxChange = (index: number) => {
-      const updatedCheckedItems = [...checkedItems];
-      updatedCheckedItems[index] = !updatedCheckedItems[index];
-      setCheckedItems(updatedCheckedItems);
-    };
+
+  const handleSearch = async (text: string) => {
+    setSearchTerm(text);
+    try {
+      const responseExercicios = await axios.get(API_URL, {
+        params: {
+          userID: user?.id,
+          data: moment().format("YYYY-MM-DD"),
+          atividadeFisica: text,
+        },
+      });
+
+      const exercicios = responseExercicios.data.map((exercicio: any) => ({
+        //id: exercicio.id,
+        //data: exercicio.data,
+        atividadeFisica: exercicio.atividadeFisica,
+        gastoCalorico: exercicio.gastoCalorico,
+        //tempo: exercicio.tempo,
+        //userID: exercicio.userID,
+      }));
+
+      setExercicios(exercicios);
+      setSelectedExercise(exercicios[0]);
+    } catch (error) {
+      console.error("Erro ao buscar dados:", error);
+    }
+  };
+
+  const handleExerciseClick = (exercise: Exercicios) => {
+    setSelectedExercise(exercise);
+    // Aqui você pode navegar para a tela desejada com o exercício selecionado
+    // Exemplo: navigation.navigate("DetalhesExercicio", { exercise });
+  };
 
   return (
     <View style={styles.container}>
@@ -86,29 +112,31 @@ const SeusExercicios: React.FC<Props> = ({ navigation }) => {// Estado para rast
         >
           <Image source={setaVolta} style={styles.arrow} />
         </TouchableOpacity>
-        <Text style={styles.header}>Seus Exercício</Text>
+        <Text style={styles.header}>Seus Exercícios</Text>
       </View>
       <View style={styles.searchContainer}>
         <Image source={lupa} />
-        <TextInput style={styles.searchInput} placeholder="Procurar" />
+        <TextInput style={styles.searchInput} placeholder="Procurar"
+          value={searchTerm}
+          onChange={(event) => handleSearch(event.nativeEvent.text)}
+        />
       </View>
       <ScrollView>
-        {exercicios.map(
+        {exercicios.filter((exercise) =>
+            exercise.atividadeFisica
+              .toLowerCase()
+              .includes(searchTerm.toLowerCase())
+          )
+          .map(
           ((exercicio, index) => (
-            <View key={index} style={styles.mealItem}>
+              <View key={index} style={styles.mealItem}>
+                <TouchableOpacity
+                onPress={() => handleExerciseClick(exercicio)}
+              ></TouchableOpacity>
               <View style={styles.mealInfo}>
                 <Text style={styles.mealName}>{exercicio.atividadeFisica}</Text>
                 <Text style={styles.mealDetail}>{exercicio.gastoCalorico} kcal</Text>
               </View>
-              <Checkbox
-                style={styles.checkbox}
-                value={checkedItems[index]}
-                onValueChange={() => handleCheckboxChange(index)}
-                color={checkedItems[index] ? styles.checkbox.color : undefined}
-              />
-              <TouchableOpacity>
-                <Ionicons name="add-circle-outline" size={28} color="green" />
-              </TouchableOpacity>
             </View>
           )
         ))}
